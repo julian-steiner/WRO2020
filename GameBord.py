88e833f194e30cf3f224d7b801bc6165386123f6
Julian <juli.steiner@bluewin.ch>
2020-09-23 17:05:16 +0200

    Update to gameboard class
    Added the possibility to track the sandbags in the gameboard class

--- GameBord.py
+++ GameBord.py
@@ -3,8 +3,9 @@ class Gameboard:
         self.house_c = ""
         self.bricks_c = ""
         self.humans_c = ""
+        self.sand_c = ""
         self.houses = [0, 0, 0, 0]
-        self.piles = [0, 0, 0, 0]
+        self.sand = [0, 0, 0, 0]
         self.bricks = [0, 0, 0, 0]
         self.humans = [0, 0, 0, 0]
         self.stage = 0
@@ -23,11 +24,16 @@ class Gameboard:
             for i in range(self.bricks.count(0)):
                 self.bricks[self.bricks.index(0)] = "None"
 
+        if(self.sand.count("Green") + self.sand.count("Blue") == 2 and self.sand.count(0) != 0):
+            for i in range(self.sand.count(0)):
+                self.sand[self.sand.index(0)] = "None"
+
         #Autofill Nones in Bricks and Houses
         if(self.bricks.count("Yellow") + self.bricks.count("Red") != 2 and self.bricks.count("None") < 2):
             for i in range(4):
                 if(self.houses[i] in ["Green", "Blue"]):
                     self.bricks[i] = "None"
+                    self.sand[i] = "None"
         
         if(self.humans.count("Yellow") + self.humans.count("Red") != 2 and self.humans.count("None") < 2):
             for i in range(4):
@@ -58,20 +64,37 @@ class Gameboard:
             else:
                 c = "Yellow"
             self.bricks[self.humans.index(0)] = c
+        
+        if(self.sand.count(0) == 1):
+            c = "None"
+            if(self.sand_c == "Blue"):
+                c = "Green"
+            else:
+                c = "Blue"
+            self.sand[self.sand.index(0)] = c
     
     def setBrick(self, position, color):
         self.bricks[position] = color
-        self.bricks_c = color
+        if color != "None":
+            self.bricks_c = color
         self.update()
 
     def setHouse(self, position, color):
         self.houses[position] = color
-        self.house_c = color
+        if color != "None":
+            self.house_c = color
         self.update()
 
     def setHuman(self, position, color):
         self.humans[position] = color
-        self.humans_c = color
+        if color != "None":
+            self.humans_c = color
+        self.update()
+
+    def setSand(self, position, color):
+        self.sand[position] = color
+        if color != "None":
+            self.sand_c = color
         self.update()
 
 
