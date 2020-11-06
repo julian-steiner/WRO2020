65476528b4d7c7954b97d847919f3be10a7c7fd5
SchwammPizza <ambros.anrig@gmail.com>
2020-11-06 21:27:16 +0100

    Worst Case

--- DeichHandling.py
+++ DeichHandling.py
@@ -34,7 +34,7 @@ class DeichHandler:
         self.DriveTrain.turnToLine((-1)**checkPoint*(self.rc.TURN_SPEED),"Black")
         return color
     
-    def scanHumans(self,checkPoint, angle):
+    def scanHumans(self,checkPoint, angle, scann):
         if checkPoint == 0 or checkPoint == 2:
             angle = self.DriveTrain.optimizeAngle(90 - angle)
         else:
@@ -42,39 +42,41 @@ class DeichHandler:
         self.DriveTrain.turnAngle(self.rc.TURN_SPEED, angle)
         self.DriveTrain.center("Black")
         self.DriveTrain.driveToLine(self.rc.SPEED, self.rc.LINE)
-        self.DriveTrain.driveForward(self.rc.SLOW_SPEED, 15.5)
-        sleep(0.5)
-        yellow = [34, 14, 8]
-        red = [36, 10, 11]
-        c_color = self.Gripper.RomerColor(red ,yellow, [0, 0, 1], "Red", "Yellow", "None")
-        if(c_color == "None"):
-            self.DriveTrain.driveForward(self.rc.APPROACH_SPEED, 1)
-            c_color = self.Gripper.RomerColor(red, yellow, [0, 0, 1], "Red", "Yellow", "None")
-        log(Motors.Gripper1.colorSensor.rgb, "RGB Value of the front sensor")
-        log(c_color, "Color of the front sensor")
-        Gameboard.setHuman(checkPoint, c_color)
-        self.DriveTrain.driveForward(self.rc.SLOW_SPEED, -4)
+        self.DriveTrain.driveForward(self.rc.SLOW_SPEED, 11.5)
+        if scann == True:
+            self.DriveTrain.driveForward(self.rc.SLOW_SPEED, 4)
+            sleep(0.5)
+            yellow = [34, 14, 8]
+            red = [36, 10, 11]
+            c_color = self.Gripper.RomerColor(red ,yellow, [0, 0, 1], "Red", "Yellow", "None")
+            if(c_color == "None"):
+                self.DriveTrain.driveForward(self.rc.APPROACH_SPEED, 1)
+                c_color = self.Gripper.RomerColor(red, yellow, [0, 0, 1], "Red", "Yellow", "None")
+            log(Motors.Gripper1.colorSensor.rgb, "RGB Value of the front sensor")
+            log(c_color, "Color of the front sensor")
+            Gameboard.setHuman(checkPoint, c_color)
+            self.DriveTrain.driveForward(self.rc.SLOW_SPEED, -4)
 
-    def scanBlocks(self, checkPoint):
+    def scanBlocks(self, checkPoint, scann):
         männli = Gameboard.humans
-# Drehen
         if checkPoint == 0 or checkPoint == 2:
             angle = -90
         else:
             angle = 90
         self.DriveTrain.turnAngle(self.rc.TURN_SPEED, angle)
         sleep(1)
-# Zu Betonblöcke fahren
         self.DriveTrain.driveForward(self.rc.SPEED, 17)
         self.DriveTrain.driveForward(self.rc.SLOW_SPEED,-95)
-        c_color = self.Gripper2.RomerColorPU()
-        log(Motors.Gripper2.colorSensor.rgb, "RGB Value of the back sensor")
-        log(c_color, "Color of the back sensor")
         if checkPoint % 2 == 1:
             checkPointz = checkPoint - 1
         if checkPoint % 2 == 0:
-            checkPointz = checkPoint + 1     
-
+            checkPointz = checkPoint + 1
+        if scann == True:
+            c_color = self.Gripper2.RomerColorPU()
+            log(Motors.Gripper2.colorSensor.rgb, "RGB Value of the back sensor")
+            log(c_color, "Color of the back sensor")
+        else:
+            c_color = Gameboard.bricks[checkPointz]
         Gameboard.setBrick(checkPointz, c_color)
         RobotContainer.setLoaded(0, c_color)
         if c_color in männli and c_color != "None":
@@ -84,21 +86,17 @@ class DeichHandler:
         sleep(0.2)
         return self.driveToPoint(checkPoint, checkPointz)
 
-# Zur mitte fahren
-
-    def driveToPoint(self, checkPoint, checkPointz):
+    def driveToPoint(self, checkPointz):
         sleep(0.5)
         self.DriveTrain.followLine(self.rc.SPEED,self.rc.AGGRESSION,self.rc.LINE,9)
-        self.DriveTrain.turnAngle(self.rc.TURN_SPEED,90*(-1)**(checkPoint + 1))
-        self.DriveTrain.center("Black", direction='-1')
+        self.DriveTrain.turnAngle(self.rc.TURN_SPEED,90*(-1)**(checkPointz))
+        # self.DriveTrain.center("Black", direction='-1')
         self.DriveTrain.followLine(self.rc.SPEED,self.rc.AGGRESSION,self.rc.LINE,20)
-        if checkPoint in [0, 2]:
+        if checkPointz in [0, 2]:
             offset = -90
         else:
             offset = 90
         self.baghandler.pickUp(checkPointz, offset)
-        
-
         return checkPointz
     
     def DeichPutDown(self, checkPoint, dislocated = 0):
@@ -127,102 +125,32 @@ class DeichHandler:
             Gameboard.setBlockDelivered(color)
         else:
             print("Color not in humans")
-
-        elif startpunkt == 1:
-            if endpunkt == 0:
-                self.turnAngle(self.rc.TURN_SPEED, 180 - s_offset)
-                self.followToLine(self.rc.SPEED, self.rc.AGGRESSION, self.rc.BLUELINE, self.rc.LINE)
-                self.followLine(self.rc.SLOW_SPEED, self.rc.AGGRESSION, self.rc.BLUELINE, end_distance)
-                self.turnAngle(self.rc.TURN_SPEED,e_offset)
-            elif endpunkt == 2:
-                self.turnAngle(self.rc.TURN_SPEED, 180 - s_offset)
-                self.followLine(self.rc.SPEED, self.rc.AGGRESSION, self.rc.BLUELINE, 25)
-                self.turnAngle(self.rc.TURN_SPEED, -90)
-                self.driveForward(self.rc.SPEED, long_distance)
-                self.turnAngle(self.rc.TURN_SPEED, -90)
-                self.followToLine(self.rc.SPEED, self.rc.AGGRESSION, self.rc.REDLINE, self.rc.LINE)
-                self.followLine(self.rc.SLOW_SPEED, self.rc.AGGRESSION, self.rc.REDLINE, end_distance)
-                self.turnAngle(self.rc.TURN_SPEED, e_offset)
-            elif endpunkt == 3:
-                self.turnAngle(self.rc.TURN_SPEED, 180 - s_offset)
-                self.followLine(self.rc.SPEED, self.rc.AGGRESSION, self.rc.BLUELINE, 25)
-                self.turnAngle(self.rc.TURN_SPEED, self.optimizeAngle(-90))
-                self.driveForward(self.rc.SPEED, long_distance)
-                self.turnAngle(self.rc.TURN_SPEED, 90)
-                self.followToLine(self.rc.SPEED, self.rc.AGGRESSION, self.rc.REDLINE, self.rc.LINE)
-                self.followLine(self.rc.SLOW_SPEED, self.rc.AGGRESSION, self.rc.REDLINE, end_distance)
-                self.turnAngle(self.rc.TURN_SPEED, e_offset)
-            elif endpunkt == 6:
-                self.turnAngle(self.rc.TURN_SPEED, 180 - s_offset)
-                self.followLine(self.rc.SPEED, self.rc.AGGRESSION, self.rc.BLUELINE, 27)
-                self.turnAngle(self.rc.TURN_SPEED, self.optimizeAngle(-90))
-                self.driveForward(self.rc.SPEED, 24)
-
-        if startpunkt == 2:
-            if endpunkt == 3:
-                self.turnAngle(self.rc.TURN_SPEED, 180 - s_offset)
-                self.followToLine(self.rc.SPEED, self.rc.AGGRESSION, self.rc.REDLINE, self.rc.LINE)
-                self.followLine(self.rc.SLOW_SPEED, self.rc.AGGRESSION, self.rc.REDLINE, end_distance)
-                self.turnAngle(self.rc.TURN_SPEED,e_offset)
-            elif endpunkt == 0:
-                self.turnAngle(self.rc.TURN_SPEED, 180 - s_offset)
-                self.followLine(self.rc.SPEED, self.rc.AGGRESSION, self.rc.REDLINE, 25)
-                self.turnAngle(self.rc.TURN_SPEED, 90)
-                self.driveForward(self.rc.SPEED, long_distance)
-                self.turnAngle(self.rc.TURN_SPEED, -90)
-                self.followToLine(self.rc.SPEED, self.rc.AGGRESSION, self.rc.BLUELINE, self.rc.LINE)
-                self.followLine(self.rc.SLOW_SPEED, self.rc.AGGRESSION, self.rc.BLUELINE, end_distance)
-                self.turnAngle(self.rc.TURN_SPEED, e_offset)
-            elif endpunkt == 1:
-                self.turnAngle(self.rc.TURN_SPEED, 180 - s_offset)
-                self.followLine(self.rc.SPEED, self.rc.AGGRESSION, self.rc.REDLINE, 25)
-                self.turnAngle(self.rc.TURN_SPEED, 90)
-                self.driveForward(self.rc.SPEED, long_distance)
-                self.turnAngle(self.rc.TURN_SPEED, 90)
-                self.followToLine(self.rc.SPEED, self.rc.AGGRESSION, self.rc.BLUELINE, self.rc.LINE)
-                self.followLine(self.rc.SLOW_SPEED, self.rc.AGGRESSION, self.rc.BLUELINE, end_distance)
-                self.turnAngle(self.rc.TURN_SPEED, e_offset)
-            elif endpunkt == 6:
-                self.turnAngle(self.rc.TURN_SPEED, 180 - s_offset)
-                self.followLine(self.rc.SPEED, self.rc.AGGRESSION, self.rc.REDLINE, 25)
-                self.turnAngle(self.rc.TURN_SPEED, 90)
-                self.driveForward(self.rc.SPEED, 100)
-
-        if startpunkt == 3:
-            if endpunkt == 2:
-                self.turnAngle(self.rc.TURN_SPEED, 180 - s_offset)
-                self.followToLine(self.rc.SPEED, self.rc.AGGRESSION, self.rc.REDLINE, self.rc.LINE)
-                self.followLine(self.rc.SLOW_SPEED, self.rc.AGGRESSION, self.rc.REDLINE, end_distance)
-                self.turnAngle(self.rc.TURN_SPEED, e_offset)
-            elif endpunkt == 0:
-                self.turnAngle(self.rc.TURN_SPEED, 180 - s_offset)
-                self.followLine(self.rc.SPEED, self.rc.AGGRESSION, self.rc.REDLINE, 25)
-                self.turnAngle(self.rc.TURN_SPEED, -90)
-                self.driveForward(self.rc.SPEED, long_distance)
-                self.turnAngle(self.rc.TURN_SPEED, -90)
-                self.followToLine(self.rc.SPEED, self.rc.AGGRESSION, self.rc.BLUELINE, self.rc.LINE)
-                self.followLine(self.rc.SLOW_SPEED, self.rc.AGGRESSION, self.rc.BLUELINE, end_distance)
-                self.turnAngle(self.rc.TURN_SPEED, e_offset)
-            elif endpunkt == 1:
-                self.turnAngle(self.rc.TURN_SPEED, 180 - s_offset)
-                self.followLine(self.rc.SPEED, self.rc.AGGRESSION, self.rc.REDLINE, 25)
-                self.turnAngle(self.rc.TURN_SPEED, -90)
-                self.driveForward(self.rc.SPEED, long_distance)
-                self.turnAngle(self.rc.TURN_SPEED, 90)
-                self.followToLine(self.rc.SPEED, self.rc.AGGRESSION, self.rc.BLUELINE, self.rc.LINE)
-                self.followLine(self.rc.SLOW_SPEED, self.rc.AGGRESSION, self.rc.BLUELINE, end_distance)
-                self.turnAngle(self.rc.TURN_SPEED, e_offset)
-            elif endpunkt == 6:
-                self.turnAngle(self.rc.TURN_SPEED, 180 - s_offset)
-                self.followLine(self.rc.SPEED, self.rc.AGGRESSION, self.rc.REDLINE, 25)
-                self.turnAngle(self.rc.TURN_SPEED, -90)
-                self.driveForward(self.rc.SPEED, 100)
-
-
-#gripper2.movemotor(50,True)
-
-#while True:
-#     print(gripper2.RomerColorPD())
-#    print(motors.Gripper2.colorSensor.rgb)
-
-#print(deichhandler.männlidriver(0,[]))
+    
+    def WorstCase(self,checkPoint):
+        self.DriveTrain.turnToLine(self.rc.TURN_SPEED*(-1)**(checkPoint + 1),"Black")
+        self.DriveTrain.turnAngle(self.rc.TURN_SPEED,(-1)**checkPoint*9)
+        sleep(0.2)
+        self.DriveTrain.driveForward(self.rc.SPEED,-26.5)
+        self.Gripper.moveMotor(100,-210)
+        self.DriveTrain.turnAngle(self.rc.SLOW_TURN_SPEED,(-1)**(checkPoint + 1)*130)
+        self.DriveTrain.turnAngle(self.rc.SLOW_TURN_SPEED,(-1)**(checkPoint)*40)
+        sleep(0.2)
+        self.Gripper.moveMotor(100,210)
+        sleep(0.2)
+# Scannen
+        self.DriveTrain.driveForward(self.rc.SLOW_SPEED,-23)
+        c_color = self.Gripper2.RomerColorPU()
+        Gameboard.setBrick(checkPoint,c_color)
+# Drehen
+        self.DriveTrain.driveForward(self.rc.SLOW_SPEED,23)
+        self.DriveTrain.turnAngle(self.rc.SLOW_TURN_SPEED,(-1)**(checkPoint)*173.5)
+# Seite wechslen
+        self.DriveTrain.driveForward(self.rc.SLOW_SPEED,-85)
+        if checkPoint % 2 == 1:
+            checkPointz = checkPoint - 1
+        if checkPoint % 2 == 0:
+            checkPointz = checkPoint + 1
+        color = Gameboard.bricks[checkPointz]
+        if color in Gameboard.humans:
+            self.Gripper2.movemotor(100,True)
+        self.driveToPoint(checkPointz)
